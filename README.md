3fbe9e7b9c7b048fc95c6368ea09403095ee1fad
cosmos <sensewan@gmail.com>
2021-08-31 16:08:30 +0900

    fitst commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# custom_dropdown_button
+
+A new Flutter project.
+
+## Getting Started
+
+## Flutter version
+2.2.0
---
+++ android/app/src/main/kotlin/com/example/custom_dropdown_button/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.custom_dropdown_button
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
